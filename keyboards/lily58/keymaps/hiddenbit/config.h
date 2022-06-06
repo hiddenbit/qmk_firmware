41c80692e21b1e0a60eb85495dbbc79e076a7ab9
hiddenbit <1763198+hiddenbit@users.noreply.github.com>
2022-06-06 14:55:59 +0200

    Use MASTER_RIGHT

--- keyboards/lily58/keymaps/hiddenbit/config.h
+++ keyboards/lily58/keymaps/hiddenbit/config.h
@@ -4,6 +4,7 @@ This is the c configuration file for the keymap
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
 Copyright 2020 Ben Roesner (keycapsss.com)
+Copyright 2022 Hiddenbit
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* Select hand configuration */
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #ifdef RGBLIGHT_ENABLE

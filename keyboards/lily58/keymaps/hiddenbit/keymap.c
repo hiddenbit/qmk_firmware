87354b432b678c3532a59e0141d77021d747d959
hiddenbit <1763198+hiddenbit@users.noreply.github.com>
2023-01-08 16:27:50 +0100

    Save current status

--- keyboards/lily58/keymaps/hiddenbit/keymap.c
+++ keyboards/lily58/keymaps/hiddenbit/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,                       KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     KC_LBRC,
     KC_ESC,   KC_A,    KC_S,     KC_D,     KC_F,     KC_G,                       KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
     KC_RSFT,  KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  MY_L_ROT,  KC_MPLY,  KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
-              KC_LCTL, KC_LALT,  KC_LGUI,  KC_SPC,   KC_ENT,    LT(_NAVIGATION, KC_SPC),   TO_SPC,   TO_MCR
+                                 KC_LCTL,  KC_LALT,  KC_LGUI, KC_SPC,  KC_ENT,   TO_NAV,   TO_SPC,   TO_MCR
     ),
 
     [_NAVIGATION] = LAYOUT(
